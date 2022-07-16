d6c0a7ee2b3013a40d81133fbee4f4be1fa6e43b
stenmarken3 <victor.stenmark22@gmail.com>
2022-07-16 22:01:41 +0200

    Yayeet

--- src/example/node.py
+++ src/example/node.py
@@ -5,12 +5,10 @@ import rospy
 def test_function():
     rospy.init_node("node")
     rate = rospy.Rate(1)
-    while not rospy.is_shutdown():
-        try:
-            print("What's up")
-            rate.sleep()
-        except rospy.ServiceException as e:
-            print("Something went wrong %s", e)
+    print("What's up")
+    rate.sleep()
+    rospy.signal_shutdown("Finished")
+    
 
 if __name__ == '__main__':
     test_function()
